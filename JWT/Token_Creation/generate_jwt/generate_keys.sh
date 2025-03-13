b58af2e23abb3df81686e566124c421eace2f048
Younes Kara Zaitri <younes.karazaitri@gmail.com>
2025-03-13 15:26:53 +0100

    fix: removal of doublons

--- JWT/Token_Creation/generate_jwt/generate_keys.sh
+++
@@ -1,4 +0,0 @@
-openssl genrsa -des3 -out private_encrypted.pem 2048
-openssl rsa -pubout -in private_encrypted.pem -out public.pem
-openssl rsa -in private_encrypted.pem -out private.pem -outform PEM
-
--- tests2/keycloak/commandes_tunnel.sh
+++
@@ -1,6 +0,0 @@
-KEYCLOAK_URL=http://$(minikube ip):$(minikube kubectl -- get services/keycloak -n keycloak -o go-template='{{(index .spec.ports 0).nodePort}}') &&
-echo "" &&
-echo "Keycloak:                 $KEYCLOAK_URL" &&
-echo "Keycloak Admin Console:   $KEYCLOAK_URL/admin" &&
-echo "Keycloak Account Console: $KEYCLOAK_URL/realms/myrealm/account" &&
-echo ""
